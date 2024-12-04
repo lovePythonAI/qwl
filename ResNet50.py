e4e83a733da1ffbdf19d86eff4d0264bfea845f4
lovePythonAI <1057012398@qq.com>
2024-12-04 23:21:07 +0800

    resnet

---
+++ ResNet50.py
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import tensorflow as tf
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.utils import to_categorical
+
+# 加载CIFAR-10数据集
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+x_train = x_train / 255
+x_test = x_test / 255
+y_train = to_categorical(y_train, num_classes=10)
+y_test = to_categorical(y_test, num_classes=10)
+
+# 加载ResNet50模型
+base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(32, 32, 3))
+
+# 添加全局平均池化层和全连接层
+model = tf.keras.models.Sequential()
+model.add(base_model)
+model.add(tf.keras.layers.GlobalAveragePooling2D())
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
+
+# 编译模型
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# 训练模型
+model.fit(x_train, y_train, batch_size=32, epochs=100, validation_data=(x_test, y_test))
+
+# 评估模型
+loss, accuracy = model.evaluate(x_test, y_test)
+print(f'Test accuracy: {accuracy}')
